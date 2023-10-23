feda8b8e3a7d9c7b5a07ec08a3517b1025304a58
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-10-23 20:25:08 +0700

    add UI for home, about, register, and login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,3 +1,32 @@
 export default function Login() {
-    return <div>Login</div>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-white text-3xl mb-4">Login</h1>
+      <form className="mt-6">
+        <div className="mb-2">
+          <label htmlFor="email" className="text-sm">
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full px-4 py-2 mt-2 text-gray-700border rounded-md focus:outline-none focus:ring"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="password" className="text-sm">
+            Password
+          </label>
+          <input
+            type="password"
+            className="w-full px-4 py-2 mt-2 text-gray-700 border rounded-md focus:outline-none focus:ring"
+          />
+        </div>
+        <div className="mt-6">
+          <button className="w-full px-4 py-2 text-white bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,3 +1,41 @@
 export default function Register() {
-    return <div>Register</div>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-white text-3xl mb-4">Register</h1>
+      <form className="mt-6">
+        <div className="mb-2">
+          <label htmlFor="username" className="text-sm">
+            Username
+          </label>
+          <input
+            type="text"
+            className="w-full px-4 py-2 mt-2 text-gray-700 rounded-md focus:outline-none focus:ring"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="email" className="text-sm">
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full px-4 py-2 mt-2 text-gray-700 rounded-md focus:outline-none focus:ring"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="password" className="text-sm">
+            Password
+          </label>
+          <input
+            type="password"
+            className="w-full px-4 py-2 mt-2 text-gray-700 rounded-md focus:outline-none focus:ring"
+          />
+        </div>
+        <div className="mt-6">
+          <button className="w-full px-4 py-2 text-white bg-gray-700 rounded-md hover:bg-gray-600">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
