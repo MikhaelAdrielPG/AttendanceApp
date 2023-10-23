884a519dd8ff65d5c7b5a7831cc7d4614ff563fc
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-10-23 20:08:42 +0700

    fix about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
-export default function About() {
+export default function Home() {
   return (
-    <div className="container">
-      <nav className="bg-gray-800 p-4 mx-auto max-w-screen-md">
-        <div className="container flex justify-between items-center">
+    <div className="container mx-auto">
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
           <a href="/" className="text-white text-xl font-bold">
             Your Website
           </a>
