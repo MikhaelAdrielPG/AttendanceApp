cf985da5b041794a2b26708faf59f2bbc9672e54
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-10-23 18:53:28 +0700

    add UI for homepage, about, login, and register page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function About() {
   return (
-    <div className="container mx-auto">
-      <nav className="bg-gray-800 p-4">
-        <div className="container mx-auto flex justify-between items-center">
+    <div className="container">
+      <nav className="bg-gray-800 p-4 mx-auto max-w-screen-md">
+        <div className="container flex justify-between items-center">
           <a href="/" className="text-white text-xl font-bold">
             Your Website
           </a>
@@ -13,14 +13,13 @@ export default function Home() {
             <a href="/about" className="text-white">
               About
             </a>
-          
           </div>
         </div>
       </nav>
 
       <div className="mt-8 text-center">
-        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-        <p className="mt-4 text-lg">This is the home page of your website.</p>
+        <h1 className="text-4xl font-bold">About Us</h1>
+        <p className="mt-4 text-lg">This is the about page of your website.</p>
       </div>
     </div>
   );
