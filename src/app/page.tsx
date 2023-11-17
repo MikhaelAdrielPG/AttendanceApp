d13b5e723d8994ba2c671a4dc5d7e761762b2b35
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-11-17 22:38:46 +0700

    add attendance table to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,29 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get<User[]>("/api/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div className="container mx-auto">
       <nav className="bg-gray-800 p-4">
@@ -6,20 +31,48 @@ export default function Home() {
           <a href="/" className="text-white text-xl font-bold">
             Attendance App
           </a>
-          <div className="flex space-x-4">
+          <div className="hidden md:flex space-x-4">
             <a href="/" className="text-white">
               Home
             </a>
             <a href="/about" className="text-white">
               About
             </a>
+            {/* Add other navigation links here */}
           </div>
         </div>
       </nav>
 
-      <div className="mt-8 text-center">
-        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-        <p className="mt-4 text-lg">This is the home page of your website.</p>
+      <div className="mt-8 mx-4 md:mx-auto md:max-w-4xl">
+        <h1 className="text-4xl font-bold">Daftar Pengguna</h1>
+        <div className="overflow-x-auto mt-4">
+          <div className="-mx-4 sm:-mx-0">
+            <table className="w-full table-auto sm:overflow-hidden border-collapse border border-gray-800">
+              <thead className="bg-black-200">
+                <tr>
+                  <th className="border border-gray-600 px-4 py-2 sm:w-1/2 md:w-auto lg:w-auto">
+                    Username
+                  </th>
+                  <th className="border border-gray-600 px-4 py-2 sm:w-1/2 md:w-auto lg:w-auto">
+                    Email
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td className="border border-gray-600 px-4 py-2 sm:w-1/2 md:w-auto lg:w-auto">
+                      {user.name}
+                    </td>
+                    <td className="border border-gray-600 px-4 py-2 sm:w-1/2 md:w-auto lg:w-auto">
+                      {user.email}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
